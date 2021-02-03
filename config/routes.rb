9e87f1a7148167da7642b900fa5f0fa05347096b
ohnishi taisei <torasora@ohnishitaiseiMacBook-Pro.local>
2021-02-03 20:32:27 +0900

    postsのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  #↓トップページは後で変更する。
+  root "posts#index"
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts
 end
