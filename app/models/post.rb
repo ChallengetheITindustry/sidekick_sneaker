3616af35246054bf11ad96f7312f27c7b67783dd
ohnishi taisei <torasora@ohnishitaiseiMacBook-Pro.local>
2021-02-09 23:58:24 +0900

    ビューにいいね欄を表示する

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,6 @@ class Post < ApplicationRecord
     validates :content, presence: true
   # post を user が「いいね！」しているときは「true」，「いいね」していないときは「false」↓
     def liked_by?(user)
-        likes.find_by(user_id: user.id).present?
+        post_likes.find_by(user_id: user.id).present?
     end
 end
