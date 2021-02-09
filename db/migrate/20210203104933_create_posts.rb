eef4b68df1bf64f14094886f92532ee31d7ec29a
ohnishi taisei <torasora@ohnishitaiseiMacBook-Pro.local>
2021-02-09 21:07:16 +0900

    postモデルにuser_idを持たせるためのカラムを追加

--- db/migrate/20210203104933_create_posts.rb
+++ db/migrate/20210203104933_create_posts.rb
@@ -1,8 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.string :image
-      t.text :content
+      t.string :image, null: false
+      t.text :content, null: false
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
