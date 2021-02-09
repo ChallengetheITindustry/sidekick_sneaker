0e0afb183de371804d62698eb473de60eedc7edb
ohnishi taisei <torasora@ohnishitaiseiMacBook-Pro.local>
2021-02-10 00:26:05 +0900

    ビューにいいねのリンク先を指定し、createとdestoyで星マークを変更できるように設定

--- app/controllers/post_likes_controller.rb
+++ app/controllers/post_likes_controller.rb
@@ -1,11 +1,11 @@
 class PostLikesController < ApplicationController
-    def create
-        current_user.likes.create!(post_id: params[:post_id])
-        redirect_back(fallback_location: root_path)
-    end
+  def create
+    current_user.post_likes.create!(post_id: params[:post_id])
+    redirect_back(fallback_location: root_path)
+  end
 
   def destroy
-    current_user.likes.find_by(post_id: params[:post_id]).destroy!
+    current_user.post_likes.find_by(post_id: params[:post_id]).destroy!
     redirect_back(fallback_location: root_path)
   end
 end
