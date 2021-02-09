1b4cc261ff4b82a74cc74ec60e112c2ea19daa97
ohnishi taisei <torasora@ohnishitaiseiMacBook-Pro.local>
2021-02-10 00:36:57 +0900

    ビューにログインしているユーザーのidの受け取りがうまくいってなかったため修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
     has_many :liked_users, through: :post_likes, source: :user
     validates :content, presence: true
   # post を user が「いいね！」しているときは「true」，「いいね」していないときは「false」↓
-    def liked_by?(user)
+    def liked_by?(current_user)
         post_likes.find_by(user_id: user.id).present?
     end
 end
