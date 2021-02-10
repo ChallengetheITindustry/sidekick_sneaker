1251c8baa8978ffd6388cca1f148a951b9dc72b5
ohnishi taisei <torasora@ohnishitaiseiMacBook-Pro.local>
2021-02-10 23:20:38 +0900

    commentコントローラーを設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,28 @@
 class CommentsController < ApplicationController
+
+    def create
+        @comment = Comment.new(comment_params)
+        @comment.user_id = current_user.id
+        if @comment.save
+            redirect_back(fallback_location: root_path)
+        else
+            redirect_back(fallback_location: root_path)
+        end
+    end
+
+    def destroy
+        comment = Comment.find_by(params[:id])
+        comment.destroy!
+        redirect_back(fallback_location: root_pathå)
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content)
+    end
+
+
+
+    
 end
