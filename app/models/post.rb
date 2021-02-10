c367de769d7cd93844f0b743fb887ae4ee1ce363
ohnishi taisei <torasora@ohnishitaiseiMacBook-Pro.local>
2021-02-10 23:12:50 +0900

    commentとuser,postを関連付け

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
     belongs_to :user
     has_many :post_likes, dependent: :destroy
+    has_many :comments
   # post.liked_users で post を「いいね!」しているユーザーの一覧を取得できるようになる↓
     has_many :liked_users, through: :post_likes, source: :user
     validates :content, presence: true
