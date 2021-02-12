d1e7feadcff763550f892426e1cb69ee43a7054d
ohnishi taisei <torasora@ohnishitaiseiMacBook-Pro.local>
2021-02-12 11:18:27 +0900

    自分の投稿を削除できるように修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
     belongs_to :user
     has_many :post_likes, dependent: :destroy
-    has_many :comments
+    has_many :comments, dependent: :destroy
   # post.liked_users で post を「いいね!」しているユーザーの一覧を取得できるようになる↓
     has_many :liked_users, through: :post_likes, source: :user
     validates :content, presence: true
