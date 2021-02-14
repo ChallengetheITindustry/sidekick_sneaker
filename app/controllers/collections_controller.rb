593615dc052c0eab956a93e38225547748b2d511
ohnishi taisei <torasora@ohnishitaiseiMacBook-Pro.local>
2021-02-15 00:33:03 +0900

    collectionコントローラーを設定

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,2 +1,31 @@
 class CollectionsController < ApplicationController
+  def index
+    @collection = Collection.find(params[:id])
+  end
+
+  def show
+    @collection = Collection.find(params[:id])
+  end
+
+  def create
+    @collection = Collection.creaate(collection_params)
+  end
+
+  def update
+    collection = Collection.find(params[:id])
+    collection.update
+    redirect_to collection_index_path
+  end
+
+  def destroy
+    collection = Collection.find(params[:id])
+    collection.destroy
+    redirect_to collection_index_path
+  end
+
+  private
+
+  def collection_params
+    params.require(:collection).permit(:image, :name, :explanation)
+  end
 end
