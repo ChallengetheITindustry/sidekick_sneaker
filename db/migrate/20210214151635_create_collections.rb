86a44e421e353927aa96d73d3c2a4aa89789d19b
ohnishi taisei <torasora@ohnishitaiseiMacBook-Pro.local>
2021-02-15 00:21:44 +0900

    null制約を設定し、マイグレーションを実行

--- db/migrate/20210214151635_create_collections.rb
+++ db/migrate/20210214151635_create_collections.rb
@@ -1,8 +1,8 @@
 class CreateCollections < ActiveRecord::Migration[6.1]
   def change
     create_table :collections do |t|
-      t.string :image
-      t.string :name
+      t.string :image, null: false
+      t.string :name, null: false
       t.text :explanation
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_11_022032) do
+ActiveRecord::Schema.define(version: 2021_02_14_151635) do
 
   create_table "active_admin_comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "namespace"
@@ -36,6 +36,14 @@ ActiveRecord::Schema.define(version: 2021_02_11_022032) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "collections", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "image", null: false
+    t.string "name", null: false
+    t.text "explanation"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -65,6 +73,14 @@ ActiveRecord::Schema.define(version: 2021_02_11_022032) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "relationships", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "following_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follower_id", "following_id"], name: "index_relationships_on_follower_id_and_following_id", unique: true
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
