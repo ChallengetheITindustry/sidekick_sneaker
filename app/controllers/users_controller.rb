0563381fc2a0415739f3b1bdefeace4002b33d89
ohnishi taisei <torasora@ohnishitaiseiMacBook-Pro.local>
2021-02-19 22:31:48 +0900

    お気に入りした投稿の画像、ユーザー名、投稿内容を表示、スタイルを調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.all
     @liked_posts = current_user.liked_posts.page(params[:page]).per(5)
+    @post = Post.all
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,14 @@
 class Post < ApplicationRecord
-    belongs_to :user
-    has_many :post_likes, dependent: :destroy
-    has_many :comments, dependent: :destroy
+  belongs_to :user
+  has_many :post_likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
   # post.liked_users で post を「いいね!」しているユーザーの一覧を取得できるようになる↓
-    has_many :liked_users, through: :post_likes, source: :user
-    validates :content, presence: true
+  has_many :liked_users, through: :post_likes, source: :user
+  validates :content, presence: true
   # post を user が「いいね！」しているときは「true」，「いいね」していないときは「false」↓
-    def liked_by?(user)
-        post_likes.find_by(user_id: user.id).present?
-    end
+  def liked_by?(user)
+    post_likes.find_by(user_id: user.id).present?
+  end
 
-    mount_uploader :image, ImageUploader
+  mount_uploader :image, ImageUploader
 end
