02d04a6d6a11dd6df8d9f006e776139e4fbc6e6a
ohnishi taisei <torasora@ohnishitaiseiMacBook-Pro.local>
2021-02-20 14:35:23 +0900

    マイグレーションファイルを修正

--- db/migrate/20210220053248_create_like_lists.rb
+++ db/migrate/20210220053248_create_like_lists.rb
@@ -1,9 +1,9 @@
 class CreateLikeLists < ActiveRecord::Migration[6.1]
   def change
     create_table :like_lists do |t|
-      t.string :list_name
-      t.reference :user_id
-      t.reference :sneaker_id
+      t.string :list_name, null: false
+      t.references :user_id
+      t.references :sneaker_id
 
       t.timestamps
     end
