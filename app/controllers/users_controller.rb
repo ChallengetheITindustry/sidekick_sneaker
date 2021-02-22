883dbdaef9bc02d9d63393ca371f937025b6fb83
ohnishi taisei <torasora@ohnishitaiseiMacBook-Pro.local>
2021-02-22 19:56:32 +0900

    ユーザーページからお気に入りした詳細画面に遷移できるように設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,5 @@ class UsersController < ApplicationController
   def show
     @user = User.all
     @liked_posts = current_user.liked_posts.page(params[:page]).per(5)
-    @post = Post.all
   end
 end
