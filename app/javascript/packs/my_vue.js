e91ec0529bf4e70989a724e0c0116f9559d19792
onishi taisei <reeksge@gmail.com>
2021-03-03 10:02:16 +0900

    リファクタリング

--- app/javascript/packs/my_vue.js
+++ app/javascript/packs/my_vue.js
@@ -2,26 +2,26 @@ import Vue from 'vue/dist/vue.esm'
 import App from '../app.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
-const vue = new Vue({
-  el: '#app',
-  data: {
-    gnav: false
-  },
-  methods: {
-    show: (e) => {
-      let elm = e.currentTarget;
-      let className = elm.className;
-      const body = document.getElementById('hidden')
-      if (className.indexOf('is-open') != -1) {
-        vue.$data.gnav = false;
-        elm.className = 'button';
-        body.className -= " non-scroll ";
-      } else {
-        vue.$data.gnav = true;
-        elm.className += " is-open";
-        body.className += " non-scroll ";
+  const vue = new Vue({
+    el: '#app',
+    data: {
+      gnav: false
+    },
+    methods: {
+      show: (e) => {
+        let elm = e.currentTarget;
+        let className = elm.className;
+        const body = document.getElementById('hidden')
+        if (className.indexOf('is-open') != -1) {
+          vue.$data.gnav = false;
+          elm.className = 'button';
+          body.className -= " non-scroll ";
+        } else {
+          vue.$data.gnav = true;
+          elm.className += " is-open";
+          body.className += " non-scroll ";
+        }
       }
     }
-  }
-})
+  })
 })
